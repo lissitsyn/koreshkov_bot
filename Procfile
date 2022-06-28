a7f8e3bcec59b283833fa370b9ee6d4dbea5bf2a
Vyacheslav Lisitsyn <lissitsyn@gmail.com>
2022-06-28 12:15:30 +0300

    1st deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python koreshkov.py
---
+++ koreshkov.py
@@ -0,0 +1,55 @@
+import os
+import random
+import time
+
+from dotenv import load_dotenv
+from telegram.ext import Updater, Filters, MessageHandler, MessageFilter
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+updater = Updater(token=TELEGRAM_TOKEN)
+
+AMERICAN_CITIES = ['Нью-Йорк',
+                   'Лос-Анджелес',
+                   'Чикаго',
+                   'Хьюстон',
+                   'Филадельфия',
+                   'Финикс',
+                   'Сан-Антонио',
+                   'Сан-Диего',
+                   'Даллас',
+                   'Сан-Хосе',]
+
+
+class FilterAmerica(MessageFilter):
+    def filter(self, message):
+        return 'Америк' in message.text
+
+
+class FilterConst(MessageFilter):
+    def filter(self, message):
+        return 'Костя' in message.text or 'Корешков' in message.text
+
+
+def who_said_america(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text=f'Кто сказал про Америку?? Я как раз собираюсь в {AMERICAN_CITIES[random.randint(0, len(AMERICAN_CITIES))]}')
+
+def kostya(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Хватит флудить, я в Америке!')
+
+filter_america = FilterAmerica()
+filter_kostya = FilterConst()
+
+def main():
+    updater.dispatcher.add_handler(MessageHandler(filter_america, who_said_america))
+    updater.dispatcher.add_handler(MessageHandler(filter_kostya, kostya))
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
